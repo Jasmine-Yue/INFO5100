021d2950022414bd436c9756e85bbddb62e89e13
Jasmine-Yue <90739235+Jasmine-Yue@users.noreply.github.com>
2021-10-29 16:57:18 -0700

    Add java files

---
+++ Assign3/Assign3-1/src/HappyObject.java
@@ -0,0 +1,22 @@
+public class HappyObject extends MoodyObject {
+
+
+  //
+    @Override
+    //returns string indicating sad
+    public String getMood() {
+        return "happy";
+    }
+
+    //test
+
+    @Override
+    //print crying string “‘waah’ ‘boo hoo’ ‘weep’ ‘sob’”
+    public void ExpressFeelings() {
+        System.out.println(" heeehee….hahahah…HAHAHA!! ");
+    }
+
+    public String toString(){
+        return "Subject laughs a lot";
+    }
+}
