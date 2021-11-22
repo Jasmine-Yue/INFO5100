9f3d7060af448a0e6fdc1e7dab9b9f435a1941b5
Jasmine-Yue <90739235+Jasmine-Yue@users.noreply.github.com>
2021-11-22 15:09:09 -0800

    Solutions for Assignment5 Problem2

---
+++ Assignment5/Problem2/src/FacadePatternDemo.java
@@ -0,0 +1,11 @@
+public class FacadePatternDemo {
+
+    public static void main(String[] args) {
+	// write your code here
+        ShapeMaker shapeMaker = new ShapeMaker();
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+
+    }
+}
